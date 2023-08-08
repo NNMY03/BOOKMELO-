f9b598d0e51e61889c6919b220808ad1a7d5b220
NNMY03 <135219119+NNMY03@users.noreply.github.com>
2023-08-08 05:30:13 +0000

    [add]customer訂正

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,35 @@
 Rails.application.routes.draw do
+
+# 管理者用
+# URL /admin/sign_in ...
+devise_for :admins, skip: [:registrations, :passwords] ,controllers: {
+  sessions: "admin/sessions"
+}
+
+# 顧客用
+
+# URL /customers/sign_in ...
+devise_for :customers, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+
+  scope module: :public do
+    root to: 'homes#top'
+    get '/attention' => 'homes#attention', as: 'attention'
+    # 会員画面
+    get   "customers/information"      => "customers#show"
+    get   "customers/information/edit" => "customers#edit"
+    patch "customers/information"      => "customers#update"
+    get   "customers/confirm_withdraw" => "customers#confirm_withdraw"
+    patch "customers/withdraw"         => "customers#withdraw"
+    # 投稿
+    resources :posts, only: [:index, :show, :edit, :new] do
+      resource :favorites, only: [:create, :destroy]
+    end
+    # 書籍検索
+    get 'books/search' => "books#serach"
+
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
