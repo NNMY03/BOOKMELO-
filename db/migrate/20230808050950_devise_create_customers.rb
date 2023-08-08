4cd562e0d64e95e6cbfc4ce4b21d6acd40237947
NNMY03 <135219119+NNMY03@users.noreply.github.com>
2023-08-08 05:39:58 +0000

    [add]customermodel add

--- db/migrate/20230808050950_devise_create_customers.rb
+++ db/migrate/20230808050950_devise_create_customers.rb
@@ -4,8 +4,14 @@ class DeviseCreateCustomers < ActiveRecord::Migration[6.1]
   def change
     create_table :customers do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
+      t.string  :name,               null: false
+      t.string  :email,              null: false, default: ""
+      t.string  :encrypted_password, null: false, default: ""
+      t.string  :age,                null: false
+      t.text    :introduction
+      t.boolean :is_deleted,         null: false, default: "false"
+      
+
 
       ## Recoverable
       t.string   :reset_password_token
