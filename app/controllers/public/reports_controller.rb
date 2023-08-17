601d1798c93e06e556f785fa0ee6abf4e9b83656
NNMY03 <135219119+NNMY03@users.noreply.github.com>
2023-08-17 11:22:46 +0000

    [add]マイグレート前

--- app/controllers/public/reports_controller.rb
+++ app/controllers/public/reports_controller.rb
@@ -8,6 +8,8 @@ class Public::ReportsController < ApplicationController
   def create
     @book = Book.find(params[:book_id])
     @report = Report.new(report_params)
+    @report.customer_id = current_customer.id  #通報者(reporter_id)にcurrent_user.idを代入
+    # @report.post_id_id = @book.id　　#通報される人(reported_id)に上で取得した@user.idを代入
     if @report.save
       redirect_to book_path(@book), notice: "ご報告ありがとうございます。"
     else
@@ -18,7 +20,7 @@ class Public::ReportsController < ApplicationController
   private
 
   def report_params
-    params.require(:report).permit(:reason, :book_id, :img_big, :author, :publisher, :item_caption)
+    params.require(:report).permit(:reason, :customer_id, :post_id, :img_big, :author, :publisher, :item_caption)
   end
 end
 
