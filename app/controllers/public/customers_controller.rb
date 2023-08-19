03fe961d91edb6f0e711a5117be89a61374346e9
NNMY03 <135219119+NNMY03@users.noreply.github.com>
2023-08-19 16:40:05 +0000

    [fix]postindex横並び

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -8,11 +8,16 @@ class Public::CustomersController < ApplicationController
     # @customer.posts # ログインユーザーに紐づくPost一覧を取得
     # pluck(:comment) # Post一覧からcommentだけ抜き出す [comment1, comment2, '', '', COMMENT3]
     # compact # 空の情報を削除する
-    @posts_latest3 = @posts.first(3)
+    @posts_latest3 = @posts.first(2)
 
     # 月別集計
-    @month_record = @posts.group("strftime('%Y%m',posts.reading_finish)").count#.count #2023/03/25
-
+    # 本番環境のみの処理
+    if Rails.env.production?
+    @month_record = @posts.group("date_format(posts.reading_finish, '%Y%m')").count #2023/03/25
+    # 開発環境のみの処理
+    elsif Rails.env.development?
+    @month_record = @posts.group("strftime('%Y%m',posts.reading_finish)").count #2023/03/25
+    end
   end
 
   def edit
