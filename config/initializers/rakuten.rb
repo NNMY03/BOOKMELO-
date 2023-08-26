74342ef5c4ce70f126724a3d847b3ed6335760c8
NNMY03 <135219119+NNMY03@users.noreply.github.com>
2023-08-26 10:24:36 +0000

    [fix]env.変更

--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,4 +1,4 @@
 RakutenWebService.configure do |c|
-  c.application_id = ENV['1061295929826067195']
-  c.affiliate_id = ENV['f1d23290ef5755ed3e43bcef1b4e83f49c48230a']
+  c.application_id = ENV['RWS_APPLICATION_ID']
+  c.affiliate_id = ENV['RWS_AFFILIATION_ID']
 end
