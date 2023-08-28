a8a5ddacf84eaf6fd02fa585aaae7bff468db48a
NNMY03 <135219119+NNMY03@users.noreply.github.com>
2023-08-28 11:34:14 +0000

    [add]favorite非同期実装

--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,18 +1,16 @@
 class Public::FavoritesController < ApplicationController
   before_action :authenticate_customer!
-  
+
   def create
-    post = Post.find(params[:post_id])
-    favorite = current_customer.favorites.new(post_id: post.id)
-     favorite.save
-     redirect_to posts_path
+    @post = Post.find(params[:post_id])
+    @favorite = current_customer.favorites.create(post_id: @post.id)
+     @favorite.save
   end
-  
+
   def destroy
-    post = Post.find(params[:post_id])
-    favorite = current_customer.favorites.find_by(post_id: post.id)
-     favorite.destroy
-     redirect_to posts_path
+    @post = Post.find(params[:post_id])
+    @favorite = current_customer.favorites.find_by(post_id: @post.id)
+     @favorite.destroy
   end
-  
+
 end
